472796e530745fa561b1593d683799d52b938c0e
İlker Gürcan Akyüz <ismailcemakyuz@aof.anadolu.edu.tr>
2022-09-28 14:30:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform_ec2_example" {
-  ami =
+  ami = "ami-062f79c1d054dd995"
   instance_type = "t2.micro"
   tags = {
     Name = "Terraform Instance"
